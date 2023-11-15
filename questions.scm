4759e5de4595ad2d5809d3352a6868c3325f8c69
Vlad Zams <pub@vladzams.com>
2023-11-15 14:40:04 -0500

    Added timeits benchmarks for empty cells detection, started [model performance evaluation] coding

---
+++ questions.scm
@@ -0,0 +1,8 @@
+
+[
+  when you use a regression model, do you follow any formal approach for picking a performance evaluation metric (or a combination of them)?  
+
+    ...MAE/MAPE, MSE, RMSE, R-squared, Adjusted R-squared – I've read through a number of overviews, all are "the best". However, I started wondering if choosing it is a matter of consensus like "use whatever my fellows used".
+
+    https://www.linkedin.com/posts/vladzams_datascience-activity-7129966707267301376-XQ_E?utm_source=share&utm_medium=member_desktop
+]
